773b418ad42c8c863fd135fbecfe83e5e8322ba3
Ludo <lpouilly88@gmail.com>
2011-12-03 19:26:21 +0100

    avec video game et new map

---
+++ content/game.php
@@ -0,0 +1,7 @@
+<object width="296" height="230">
+<param name="movie" value="http://www.youtube.com/v/yWEJ41vTqQE?version=3&amp;hl=fr_FR&autoplay=1">
+</param><param name="allowFullScreen" value="true">
+</param><param name="allowscriptaccess" value="always">
+</param><embed src="http://www.youtube.com/v/yWEJ41vTqQE?version=3&amp;hl=fr_FR&autoplay=1" type="application/x-shockwave-flash" width="296" height="230" allowscriptaccess="always" allowfullscreen="true">
+</embed>
+</object>
--- content/map.php
+++ content/map.php
@@ -3,5 +3,5 @@ Qui peut m'aider ?
 </div>
 
 <div id="carte">
-<a href="content/phraseami.html"><img src=static/img/maptest.png alt="map" width="276px" height="276"/></a>
+<a href="content/phraseami.html"><img src=static/img/map.png alt="map" width="276px" height="276"/></a>
 </div>
