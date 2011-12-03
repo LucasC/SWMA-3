32b3e3f2c3c7f71034db51d82420801114acb4fe
Ludo <lpouilly88@gmail.com>
2011-12-03 22:48:12 +0100

    22h48, bientot la nuit

--- content/game.php
+++ content/game.php
@@ -1,3 +1,8 @@
+<div id="entete">
+Tetris
+</div>
+
+<div id="video">
 <object width="296" height="230">
 <param name="movie" value="http://www.youtube.com/v/yWEJ41vTqQE?version=3&amp;hl=fr_FR&autoplay=1">
 </param><param name="allowFullScreen" value="true">
@@ -5,3 +10,4 @@
 </param><embed src="http://www.youtube.com/v/yWEJ41vTqQE?version=3&amp;hl=fr_FR&autoplay=1" type="application/x-shockwave-flash" width="296" height="230" allowscriptaccess="always" allowfullscreen="true">
 </embed>
 </object>
+</video>
