2c453c4ef848485a71772cffaa5ec964ca2899cb
Lucas <lucascerdan@gmail.com>
2011-12-03 09:59:17 +0100

    Hello World!

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
+	<head>
+	<title>Proto NoSmoke</title>
+	<link href="css/style.css" media="all" rel="stylesheet" type="text/css" />
+	<link href="css/nivo-slider.css" media="all" rel="stylesheet" type="text/css" />
+	<link media="screen" rel="stylesheet" href="css/colorbox.css" />
+	<link rel="shortcut icon" href="favicon.ico" />
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js"></script>
+</head>
+
+<body>
+hello world
+</body>
+</html>
